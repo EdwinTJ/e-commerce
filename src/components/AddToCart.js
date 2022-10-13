9c233f072c384543fb8fc28e3e0fe13db1c376a7
EdwinTJ <edwin16x@gmail.com>
2022-10-12 19:24:31 -0600

    Cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,7 @@ const AddToCart = ({ product }) => {
   // const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
 
+  const { addToCart } = useCartContext();
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
@@ -60,7 +61,7 @@ const AddToCart = ({ product }) => {
         <Link
           to="/cart"
           className="btn"
-          // onClick={() => addToCart(id, mainColor, amount, product)}
+          onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
         </Link>
